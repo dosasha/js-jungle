26788f060dc5b18f144198022c1093dcd56f7a79
Archisha Garg <157105787+dosasha@users.noreply.github.com>
2025-03-23 03:23:33 +0000

    Force saving file

--- 01_basics/01_variables_fixed.js
+++ 01_basics/01_variables_fixed.js
@@ -2,7 +2,21 @@ const accountID = 144553
 let accountEmail = "archisha@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
+let accountState;
 
-//accountID = 2;
+//accountID = 2;// not allowed
+
+accountEmail = "hc@hc.com"
+accountPssword = "212121"
+accountCity = "Mumbai"
 
 console.log(accountID);
+
+
+/*
+Prefer not to use var because of the issue in block scope 
+and functional scope.
+*/
+
+console.table([accountID,accountEmail,accountPassword,accountCity]);
+  
