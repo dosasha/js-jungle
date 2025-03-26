97675f56c37720efa656fc8f5b8921365ac71dc2
Archisha Garg <157105787+dosasha@users.noreply.github.com>
2025-03-26 04:46:06 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+//console.log(3 + 3); console.log("Archisha")// readiblity compromised - not the ideal case
+
+
+let name = "Archisha"
+let age = 19
+let isLoggedIn = false //Boolean 
+
+
+/* -- datatypes --
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value (null is an OBJECT asked about its datatype.)
+undefined => (different from null)
+symbol => unique
+object
+*/
+
+console.log(typeof undefined); //undefined 
+console.log(typeof null); //object
