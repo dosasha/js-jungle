3dfed4a161ca19674c21a3c1b65b26f217349b86
Archisha Garg <157105787+dosasha@users.noreply.github.com>
2025-03-26 05:31:00 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "Archisha"
+
+console.log(typeof score);
+console.log(typeof(score)); //writing as a method
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); //NAN is a special type to check whether it is actually a number or not.
+
+/*
+"33" => 33
+"33abc" => NaN (but type is number only)
+true => 1; false => 0; */
+
+let isLoggedIn = "Archisha"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+ ""(empty string) => false
+ "Archisha" => true
+ */
+
+ let someNumber = 33
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ 
+ 
+
+ 
